650da516243f4ee745258af791816f0ab46da25f
desmonddam <markettollls7@gmail.com>
2025-07-22 05:38:36 -0400

    add env variable

--- src/constants/BaseUrl.js
+++ src/constants/BaseUrl.js
@@ -1,6 +1,1 @@
-// const BASE_URL = "https://dev.markettoll.com/api/v1/";
-// const BASE_URL="https://backend.markettoll.com/api/v1";
-// const BASE_URL = `http://localhost:4001/api/v1`;
-const BASE_URL = "https://markettoll-backend.onrender.com/api/v1";
-
-export default BASE_URL;
+export const BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
